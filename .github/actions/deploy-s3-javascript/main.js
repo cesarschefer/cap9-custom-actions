5915b3c208aac6ba3b111d94e903b6baaedb8cd6
cesarschefer <cesarschefer@gmail.com>
2025-04-17 11:25:12 -0300

    adds real deploy to AWS

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,7 +1,16 @@
 import * as core from '@actions/core';
 
 function run() {
-  core.notice("hello from my custom JS action!")
+    // 1) Get some input values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2) Upload files
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+   
 }
 
 run();
