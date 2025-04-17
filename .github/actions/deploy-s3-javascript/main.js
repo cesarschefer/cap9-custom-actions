61c8d9c81bdc1f8eb9885fddc55b15157983b8bf
cesarschefer <cesarschefer@gmail.com>
2025-04-17 10:16:05 -0300

    adds js custom action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,8 @@
-const core = require('@actions/core');
+//const core = require('@actions/core');
 // const github = require('@actions/github');
-const exec = require('@actions/exec');
+//const exec = require('@actions/exec');
+
+import * as core from '@actions/core';
 
 function run() {
   core.notice("hello from my custom JS action!")
